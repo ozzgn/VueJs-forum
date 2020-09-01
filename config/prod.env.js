c75a91c01eee2efe2e60dd842f3d3e6d27acf667
Ozzy <ozzgn@me.com>
2020-09-01 12:23:47 -0400

    Firebase API prod env updated

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,12 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  FIREBASE_NODE_ENV: '"production"',
+  FIREBASE_API_KEY: "'AIzaSyA0V_z9a9YSTi8UWYwNEZy_31QThhMAG5Q'",
+  FIREBASE_AUTH_DOMAIN: "'oz-forum.firebaseapp.com'",
+  FIREBASE_DATABASE_URL: "'https://oz-forum.firebaseio.com'",
+  FIREBASE_PROJECT_ID: "'oz-forum'",
+  FIREBASE_STORAGE_BUCKET: "'oz-forum.appspot.com'",
+  FIREBASE_MESSAGING_ID: "'757170567581'",
+  FIREBASE_APP_ID: "'1:757170567581:web:9ff74078a469addf3a1774'",
+  FIREBASE_MEASUREMENT_ID: "'G-1ZZK8B3G2S'"
 }
--- src/main.js
+++ src/main.js
@@ -12,17 +12,16 @@ Vue.component('AppDate', AppDate)
 Vue.config.productionTip = false
 // Initialize Firebase 
 const config = {
-  apiKey: 'AIzaSyA0V_z9a9YSTi8UWYwNEZy_31QThhMAG5Q',
-  authDomain: 'oz-forum.firebaseapp.com',
-  databaseURL: 'https://oz-forum.firebaseio.com',
-  projectId: 'oz-forum',
-  storageBucket: 'oz-forum.appspot.com',
-  messagingSenderId: '757170567581',
-  appId: '1:757170567581:web:9ff74078a469addf3a1774',
-  measurementId: 'G-1ZZK8B3G2S'
+  apiKey: process.env.FIREBASE_NODE_ENV,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 firebase.initializeApp(config)
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
