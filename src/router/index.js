8825895629ca7e83887edc47879ed7dba443c8e8
Ozzy <ozzgn@me.com>
2020-09-02 09:41:12 -0400

    updated files

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import ThreadCreate from '@/pages/PageThreadCreate'
 import ThreadEdit from '@/pages/PageThreadEdit'
 import Category from '@/pages/PageCategory'
 import Forum from '@/pages/PageForum'
+import Register from '@/pages/PageRegister'
 import Profile from '@/pages/PageProfile'
 import NotFound from '@/pages/PageNotFound'
 
@@ -59,6 +60,11 @@ export default new Router({
       name: 'ProfileEdit',
       component: Profile,
       props: {edit: true}
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
     },
     {
       path: '*',
---
+++ src/store/actions.js
@@ -0,0 +1,174 @@
+import firebase from 'firebase/app'
+
+export default {
+    createPost ({commit, state}, post) {
+        const postId = firebase.database().ref('posts').push().key
+        post.userId = state.authId
+        post.publishedAt = Math.floor(Date.now() / 1000)
+
+        const updates = {}
+        updates[`posts/${postId}`] = post
+        updates[`threads/${post.threadId}/posts/${postId}`] = postId
+        updates[`threads/${post.threadId}/contributors/${post.userId}`] = post.userId
+        updates[`users/${post.userId}/posts/${postId}`] = postId
+        firebase.database().ref().update(updates)
+            .then(() => {
+                // Set post
+                commit('setItem', {resource: 'posts', item: post, id: postId})
+                // Append post to thread
+                commit('appendPostToThread', {parentId: post.threadId, childId: postId})
+                commit('appendContributorToThread', {parentId: post.threadId, childId: post.userId})
+                // Append post to user
+                commit('appendPostToUser', {parentId: post.userId, childId: postId})
+                return Promise.resolve(state.posts[postId])
+            }) 
+    },
+
+    createThread ({state, commit, dispatch}, {text, title, forumId}) {
+        return new Promise((resolve, reject) => {
+            const threadId = firebase.database().ref('threads').push().key
+            const postId = firebase.database().ref('posts').push().key
+            const userId = state.authId
+            const publishedAt = Math.floor(Date.now() / 1000)
+
+            const thread = {title, forumId, publishedAt, userId, firstPostId: postId, posts: {}}
+            thread.posts[postId] = postId
+            const post = {text, publishedAt, threadId, userId}
+
+            const updates = {}
+            // set thread
+            updates[`threads/${threadId}`] = thread
+            // append to forum
+            updates[`forums/${forumId}/threads/${threadId}`] = threadId
+            updates[`users/${userId}/threads/${threadId}`] = threadId
+            // append to user
+            updates[`posts/${postId}`] = post
+            updates[`users/${userId}/posts/${postId}`] = postId
+
+            firebase.database().ref().update(updates)
+                .then(() => {
+                    // Update threads
+                    commit('setItem', {resource: 'threads', id: threadId, item: thread})
+                    commit('appendThreadToForum', {parentId: forumId, childId: threadId})
+                    commit('appendThreadToUser', {parentId: userId, childId: threadId}) 
+                    // Update Post
+                    commit('setItem', {resource: 'posts', item: post, id: postId})
+                    commit('appendPostToThread', {parentId: post.threadId, childId: postId})
+                    commit('appendPostToUser', {parentId: post.userId, childId: postId})
+
+                    resolve(state.threads[threadId])
+                })           
+        })
+    },
+
+    createUser({state, commit}, {id, email, name, username, avatar = null}) {
+        return new Promise((resolve, reject) => {
+            const registeredAt = Math.floor(Date.now() / 1000)
+            const usernameLower = username.toLowerCase()
+            email = email.toLowerCase()
+            const user = {avatar, email, name, username, usernameLower, registeredAt}
+            firebase.database().ref('users').child(id).set(user)
+                .then(() => {
+                    commit('setItem', {resource: 'users', id: id, item: user})
+                    resolve(state.users[id])
+                })
+        })
+    },
+
+
+    updateThread ({state, commit, dispatch}, {title, text, id}) {
+        return new Promise((resolve, reject) => { 
+            const thread = state.threads[id]
+            const post = state.posts[thread.firstPostId]
+            
+            const edited = {
+                at: Math.floor(Date.now() / 1000),
+                by: state.authId
+            }
+
+            const updates = {}
+            updates[`posts/${thread.firstPostId}/text`] = text
+            updates[`posts/${thread.firstPostId}/edited`] = edited
+            updates[`threads/${id}/title`] = title
+
+            firebase.database().ref().update(updates)
+                .then(() => {
+                    commit('setThread', {thread: {...thread, title}, threadId: id})
+                    commit('setPost', {postId: thread.firstPostId, post: {...post, text, edited}})
+                    resolve(post) 
+            })
+        })
+    },
+
+    updatePost ({state, commit}, {id, text}) {
+        return new Promise((resolve, reject) => {
+            const post = state.posts[id]
+            const edited = {
+                at: Math.floor(Date.now() / 1000),
+                by: state.authId
+            }
+
+            const updates = {text, edited}
+            firebase.database().ref('posts').child(id).update(updates)
+                .then(() => {
+                    commit('setPost', {postId: id, post: {...post, text, edited}})
+                    resolve(post) 
+            })            
+        })
+    },
+
+    updateUser ({commit}, user) {
+        commit('setUser', {userId: user['.key'], user})
+    },
+
+    fetchCategory: ({dispatch}, {id}) => dispatch('fetchItem', {resource: 'categories', id, emoji: 'fetchCategory 📁'}),
+
+    fetchForum: ({dispatch}, {id}) => dispatch('fetchItem', {resource: 'forums', id, emoji: 'fetchForum 😃'}),
+
+    fetchThread: ({dispatch}, {id}) => dispatch('fetchItem', {resource: 'threads', id, emoji: 'fetchThread 🔥'}),
+
+    fetchPost: ({dispatch}, {id}) => dispatch('fetchItem', {resource: 'posts', id, emoji: 'fetchPost 📰'}),
+
+    fetchUser: ({dispatch}, {id}) => dispatch('fetchItem', {resource: 'users', id, emoji: 'fetchUser 🐔'}),
+
+
+    fetchCategories: ({dispatch}, {ids}) => dispatch('fetchItems', {resource: 'categories', ids, emoji: 'fetchCategories 📁'}),      
+
+    fetchForums: ({dispatch}, {ids}) => dispatch('fetchItems', {resource: 'forums', ids, emoji: 'fetchForums 😃'}),
+
+    fetchThreads: ({dispatch}, {ids}) => dispatch('fetchItems', {resource: 'threads', ids, emoji: 'fetchThreads 🔥'}),
+
+    fetchPosts: ({dispatch}, {ids}) => dispatch('fetchItems', {resource: 'posts', ids, emoji: 'fetchPosts 🐎'}),
+
+    fetchUsers: ({dispatch}, {ids}) => dispatch('fetchItems', {resource: 'users', ids, emoji: 'fetchUsers 🐔'}),
+
+
+    fetchAllCategories ({state, commit}) {
+        console.log('😏', '✅', 'Fetch All Categories')
+        return new Promise((resolve, reject) => {
+            firebase.database().ref('categories').once('value', snapshot => {
+                const categoriesObject = snapshot.val()
+                Object.keys(categoriesObject).forEach(categoryId => {
+                    const category = categoriesObject[categoryId]
+                    commit('setItem', {resource: 'categories', id: categoryId, item: category})
+                })
+                resolve(Object.values(state.categories))
+            })
+        })    
+    },
+
+    fetchItem ({state, commit}, {id, emoji, resource}) {
+        console.log('🌟', emoji, id)
+        return new Promise((resolve, reject) => {
+            firebase.database().ref(resource).child(id).once('value', snapshot => {
+                commit('setItem', {resource, id: snapshot.key, item: snapshot.val()})
+                resolve(state[resource][id])
+            })
+        })
+    },
+
+    fetchItems ({dispatch}, {ids, resource, emoji}) {
+        ids = Array.isArray(ids) ? ids : Object.keys(ids)
+        return Promise.all(ids.map(id => dispatch('fetchItem', {id, resource, emoji})))
+    }
+}
