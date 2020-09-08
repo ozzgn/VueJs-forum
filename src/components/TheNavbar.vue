147f84da193eeeb1f861b1a411e3d14842089889
Ozzy <ozzgn@me.com>
2020-09-08 16:27:29 -0400

    Navbar updated - Course complete! woot

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,100 +1,95 @@
 <template>
-    <header class="header" id="header">
-        <router-link
-            :to="{name: 'Home'}"
-            class="logo"
-        >
-            <img src="../assets/img/logo.png">
-        </router-link>
+  <header class="header" id="header" v-click-outside="closeMobileNavbar" v-handle-scroll="closeMobileNavbar">
 
-        <div class="btn-hamburger">
-            <!-- use .btn-humburger-active to open the menu -->
-            <div class="top bar"></div>
-            <div class="middle bar"></div>
-            <div class="bottom bar"></div>
-        </div>
+    <router-link
+      :to="{name: 'Home'}"
+      class="logo"
+    >
+      <img src="../assets/img/logo.png">
+    </router-link>
+
+    <div class="btn-hamburger" @click="mobileNavOpen = !mobileNavOpen">
+      <!-- use .btn-humburger-active to open the menu -->
+      <div class="top bar"></div>
+      <div class="middle bar"></div>
+      <div class="bottom bar"></div>
+    </div>
 
     <!-- use .navbar-open to open nav -->
-    <nav class="navbar">
-        <ul v-if="user">
-             <li class="navbar-user">
-                 <a @click.prevent="userDropdownOpen = !userDropdownOpen">
-                    <img class="avatar-small" :src="user.avatar" alt="">
-                    <span>
-                        {{user.name}}
-                        <img class="icon-profile" src="../assets/img/arrow-profile.svg" alt="">
-                    </span>
-                 </a>
+    <nav class="navbar" :class="{'navbar-open': mobileNavOpen}">
+      <ul v-if="user">
+        <li class="navbar-user" v-click-outside="closeUserDropdown">
+          <a @click.prevent="userDropdownOpen = !userDropdownOpen">
+            <img class="avatar-small" :src="user.avatar" alt="">
+            <span>
+                {{user.name}}
+                <img class="icon-profile" src="../assets/img/arrow-profile.svg" alt="">
+            </span>
+          </a>
 
-                <!-- dropdown menu -->
-                <!-- add class "active-drop" to show the dropdown -->
-                <div id="user-dropdown" :class="{'active-drop': userDropdownOpen}">
-                    <div class="triangle-drop"></div>
-                    <ul class="dropdown-menu">
-                        <li class="dropdown-menu-item">
-                            <router-link :to="{name: 'Profile'}">View Profile</router-link>
-                        </li>
-                        <li class="dropdown-menu-item">
-                            <a @click.prevent="$store.dispatch('auth/signOut')">Sign Out</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li class="navbar-item">
-                
-            </li>
-        </ul>
-        <ul v-else>
-            <li class="navbar-item">
-                <router-link :to="{name: 'SignIn'}">Sign In</router-link>
-            </li>
-            <li class="navbar-item">
-                <router-link :to="{name: 'Register'}">Register</router-link>
-            </li>
-            
-        </ul>
-
-        <!-- <ul>
-            <li class="navbar-item">
-                <a href="index.html">Home</a>
-            </li>
-            <li class="navbar-item">
-                <a href="category.html">Category</a>
-            </li>
-            <li class="navbar-item">
-                <a href="forum.html">Forum</a>
-            </li>
-            <li class="navbar-item">
-                <a href="thread.html">Thread</a>
-            </li>
-            <li class="navbar-item mobile-only">
-                <a href="profile.html">My Profile</a>
-            </li>
-            <li class="navbar-item mobile-only">
-                <a href="#">Logout</a>
-            </li>
-        </ul> -->
+          <!-- dropdown menu -->
+          <!-- add class "active-drop" to show the dropdown -->
+          <div id="user-dropdown" :class="{'active-drop': userDropdownOpen}">
+            <div class="triangle-drop"></div>
+            <ul class="dropdown-menu">
+              <li class="dropdown-menu-item">
+                <router-link :to="{name: 'Profile'}">View Profile</router-link>
+              </li>
+              <li class="dropdown-menu-item">
+                <a @click.prevent="$store.dispatch('auth/signOut')">Sign Out</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li class="navbar-mobile-item"><router-link :to="{name: 'Profile'}">View Profile</router-link></li>
+        <li class="navbar-mobile-item"><a @click.prevent="$store.dispatch('auth/signOut')">Sign Out</a></li>
+      </ul>
+      <ul v-else>
+        <li class="navbar-item">
+          <router-link :to="{name: 'SignIn'}">Sign In</router-link>
+        </li>
+        <li class="navbar-item">
+          <router-link :to="{name: 'Register'}">Register</router-link>
+        </li>
+      </ul>
     </nav>
   </header>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
-export default {
+  import {mapGetters} from 'vuex'
+  import clickOutside from '@/directives/click-outside'
+  import handleScroll from '@/directives/handle-scroll'
+  
+  export default {
+    directives: {
+      clickOutside,
+      handleScroll
+    },
+
     data () {
-        return {
-            userDropdownOpen: true
-        }
+      return {
+        userDropdownOpen: false,
+        mobileNavOpen: false
+      }
     },
 
     computed: {
-        ...mapGetters({
-            'user': 'auth/authUser'
-        })
+      ...mapGetters({
+        'user': 'auth/authUser'
+      })
+    },
+
+    methods: {
+      closeUserDropdown () {
+        this.userDropdownOpen = false
+      },
+      closeMobileNavbar () {
+        this.mobileNavOpen = false
+      }
     }
-}
+  }
 </script>
 
 <style scoped>
-
 </style>
